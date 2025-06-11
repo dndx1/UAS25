1ea68d5eea948e6cebdab752b0039bae8e7b6580
dndx1 <daniaqmaah@gmail.com>
2025-06-12 01:07:58 +0700

    first commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\ProductModel;
+class Home extends BaseController
+{
+    protected $product;
+
+   public function __construct(){
+        helper('form');
+        helper('number');
+        $this->product=new ProductModel();
+    }
+    public function index(): string
+    {
+        $product = $this->product->findAll();
+        $data['product']=$product;
+        return view('v_home', $data);
+    }
+}
